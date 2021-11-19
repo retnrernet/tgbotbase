444cc0e72dc1032443088a5d88b983b8d71c5073
retnrernet <49436790+retnrernet@users.noreply.github.com>
2021-11-19 23:25:13 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # group ids or account ids can be retrieved with @username_to_id_bot
-BOT_TOKEN="<bot-token>"
-BOT_OWNER=<bot-owner-id>
+BOT_TOKEN="1023713845:AAGZ-hHJbPfx8phFn53udo1wYVCxSVfOFH8"
+BOT_OWNER=969534165
